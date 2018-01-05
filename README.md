c2babbd80a8d352946eabdb0d630baefaa621185
Daniel Tolbert <dtolbert@bandwidth.com>
2018-01-05 16:35:22 -0500

    Simple call example

--- README.md
+++ README.md
@@ -1,3 +1,82 @@
 # node-express-101
 
-This is an app that uses express and Bandwidth to auto-respond to incoming texts and calls.
+This is a basic application that makes phone calls and speaks a sentence to a phone number.
+
+## Prereqs
+
+* [Bandwidth Account](http://dev.bandwidth.com)
+* [ngrok](http://ngrok.io) (or similar)
+* [Node v6+](https://nodejs.org/en/)
+
+## Running the example
+
+### Env Vars
+
+Be sure that the following environment variables are set. Learn how to set [envirnoment variables](https://www.schrodinger.com/kb/1842)
+
+| Varaible                 | Descirption                                              | Example                |
+|:-------------------------|:---------------------------------------------------------|:-----------------------|
+| `BANDWIDTH_PHONE_NUMBER` | Your bandwidth phone number to create the calls **FROM** | `+19197771111`         |
+| `BANDWIDTH_USER_ID`      | Your bandwidth **API** user-id                           | `u-123acbas3k3459c`    |
+| `BANDWIDTH_API_TOKEN`    | Your bandwidth API token                                 | `t-abc123asdg390856c`  |
+| `BANDWIDTH_API_SECRET`   | Your bandwidth API secret                                | `abd1239890alkjc90385` |
+
+
+### Launch ngrok
+
+[Ngrok](https://ngrok.com) is an awesome tool that lets you open up local ports to the internet.
+
+![Ngrok how](https://ngrok.com/static/img/demo.png)
+
+Once you have ngrok installed, open a new terminal tab and navigate to it's location on the file system and run:
+
+```bash
+$ cd ~/Downloads/
+$ ./ngrok http 3000
+```
+
+You'll see the terminal show you information
+
+![ngrok terminal](https://github.com/BandwidthExamples/masked-number-api/blob/master/readme_images/ngrok_terminal.png?raw=true)
+
+Copy the `http://8a543f5f.ngrok.io` link and save it for later
+
+### Install and launch the app
+
+```bash
+npm install
+npm start
+```
+
+### Create the call
+
+To create a call, we have to create a `POST` request to the service:
+
+```http
+POST http://8a543f5f.ngrok.io/create-call
+
+{
+  "phoneNumber": "+19192223333"
+}
+
+201 Created
+{
+  "from"        : "+17079311113",
+  "to"          : "+19197891146",
+  "callbackUrl" : "http://8a543f5f.ngrok.io/outbound-callbacks",
+  "id"          : "c-abc23"
+}
+
+```
+
+### Sample Curl
+
+```bash
+curl -v POST http://8a543f5f.ngrok.io/create-call \
+    -H "Content-type: application/json" \
+    -d \
+    '
+    {
+        "phoneNumber" : "+19192223333"
+    }'
+```
--- index.js
+++ index.js
@@ -1,114 +1,52 @@
-// console.log('Hello World');
 
-var Bandwidth = require("node-bandwidth");
+const Bandwidth = require("node-bandwidth");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var http = require("http").Server(app);
+let app = express();
+let http = require("http").Server(app);
 
-var myBWNumber = process.env.BANDWIDTH_PHONE_NUMBER;
+// your bandwidth phonenumber
+const myBWNumber = process.env.BANDWIDTH_PHONE_NUMBER;
 
-var myCreds = {
+// your bandwidth API creds
+const myCreds = {
     userId    : process.env.BANDWIDTH_USER_ID,
     apiToken  : process.env.BANDWIDTH_API_TOKEN,
     apiSecret : process.env.BANDWIDTH_API_SECRET
 };
 
-var client = new Bandwidth(myCreds);
+//create bandwidth client
+const client = new Bandwidth(myCreds);
 
+//Express setup
 app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 3000));
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
     //console.log(req);
     res.send("Hello World");
 });
 
-app.post("/message-callback", function (req, res) {
-    var body = req.body;
-    res.sendStatus(200);
-    if (body.direction === "in") {
-        var numbers = {
-            to: body.from,
-            from: body.to
-        }
-        sendMessage(numbers);
-    }
-});
-
-app.post("/outbound-callbacks", function (req, res) {
-    var body = req.body;
+app.post("/outbound-callbacks", (req, res) => {
+    const body = req.body;
     console.log(body);
-    if (isAnswer(body.eventType)) {
-        speackSentenceInCall(body.callId, "Hello from Bandwidth")
+    if (body.eventType === "answer") {
+        const options = {
+            sentence : "hola de Bandwidth",
+            gender   : "male",
+            locale   : "es",
+            voice    : "Jorge"
+        }
+        client.Call.playAudioAdvanced(body.callId, options)
         .then(function (response) {
+            console.log('Response from Bandwidth Server: ')
             console.log(response);
         })
         .catch(function (error) {
+            console.log('Error from Bandwidth Server: ')
             console.log(error);
         })
-    }
-    else if (isSpeakingDone(body)) {
-        client.Call.hangup(body.callId)
-        .then(function () {
-            console.log("Hanging up call");
-        })
-        .catch(function (err) {
-            console.log("Error hanging up the call, it was probably already over")
-            console.log(err);
-        });
-    }
-
-});
-
-app.post("/calls", function (req, res) {
-    var callbackUrl = getBaseUrl(req) + "/outbound-callbacks";
-    var body = req.body;
-    var phoneNumber = body.phoneNumber;
-    createCallWithCallback(phoneNumber, myBWNumber, callbackUrl)
-    .then(function (call) {
-        console.log(call);
-        res.send(call).status(201);
-    })
-    .catch(function (err) {
-        console.log(err);
-        console.log("ERROR CREATING CALL 😢");
-    });
-});
-
-var isAnswer = function (eventType) {
-    return (eventType === "answer");
-}
-
-var isSpeakingDone = function (callBackEvent) {
-    return (callBackEvent.eventType === "speak" && callBackEvent.state === "PLAYBACK_STOP");
-}
-
-var createCallWithCallback = function (toNumber, fromNumber, callbackUrl) {
-    return client.Call.create({
-        from: fromNumber,
-        to: toNumber,
-        callbackUrl: callbackUrl
-    });
-};
-
-var speackSentenceInCall = function (callId, sentence) {
-    return client.Call.speakSentence(callId, sentence);
-}
-
-app.post("/call-callback", function (req, res) {
-    var body = req.body;
-    res.sendStatus(200);
-    if (body.eventType === "answer"){
-        client.Call.speakSentence(body.callId, "I still really like dogs better")
-        .then(function () {
-            console.log("speakSentence sent");
-        })
-        .catch(function (err) {
-            console.log(err);
-        });
-
     }
     else if (body.eventType === "speak" && body.state === "PLAYBACK_STOP") {
         client.Call.hangup(body.callId)
@@ -120,47 +58,34 @@ app.post("/call-callback", function (req, res) {
             console.log(err);
         });
     }
-    else {
-        console.log(body);
-    }
 });
 
-var getBaseUrl = function (req) {
-    return 'http://' + req.hostname;
-};
-
-
-
-var messagePrinter = function (message) {
-    console.log('Using the message printer');
-    console.log(message);
-}
-
-
-var sendMessage = function (params) {
-    return client.Message.send({
-        from : params.from,
-        to   : params.to,
-        text : "404 Cats are Great, but still not as awesome as dogs",
-        media: "http://s.quickmeme.com/img/a8/a8022006b463b5ed9be5a62f1bdbac43b4f3dbd5c6b3bb44707fe5f5e26635b0.jpg"
+app.post("/create-call", function (req, res) {
+    const callbackUrl = getBaseUrl(req) + "/outbound-callbacks";
+    const body = req.body;
+    const phoneNumber = body.phoneNumber;
+    createCallWithCallback(phoneNumber, myBWNumber, callbackUrl)
+    .then(function (call) {
+        console.log(call);
+        res.send(call).status(201);
     })
-    .then(function(message){
-        messagePrinter(message);
-        return client.Message.get(message.id);
-    })
-    .then(messagePrinter)
     .catch(function (err) {
         console.log(err);
+        console.log("ERROR CREATING CALL 😢");
     });
-}
+});
 
-http.listen(app.get('port'), function(){
+const createCallWithCallback = (toNumber, fromNumber, callbackUrl) => {
+    return client.Call.create({
+        from        : fromNumber,
+        to          : toNumber,
+        callbackUrl : callbackUrl
+    });
+};
+
+const getBaseUrl = req => `http://${req.hostname}`;
+
+http.listen(app.get('port'), () => {
     console.log('listening on *:' + app.get('port'));
 });
 
-// var numbers = {
-//     from: "+13204601137",
-//     to: "+19197891146"
-// };
-
-// sendMessage(numbers);
